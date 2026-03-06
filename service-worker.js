b8b150d76063b4f5132abce9ce2b9ed7daaaa1fb
Aro5683 <arotesting54@gmail.com>
2026-03-06 09:08:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,76 +1,28 @@
-const CACHE_NAME = "bedvault-v2";
+const CACHE_NAME = "bed-vault-cache-v1";
 
-const STATIC_CACHE = [
+const urlsToCache = [
 "./",
-"./index.html",
-"./manifest.json"
+"./index.html"
 ];
 
 self.addEventListener("install", event => {
 
-self.skipWaiting();
-
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>{
-return cache.addAll(STATIC_CACHE);
-})
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
 );
 
 });
-
-self.addEventListener("activate", event => {
-
-event.waitUntil(
-caches.keys().then(keys=>{
-return Promise.all(
-keys.filter(key=>key!==CACHE_NAME)
-.map(key=>caches.delete(key))
-)
-})
-);
-
-});
-
-/* FETCH */
 
 self.addEventListener("fetch", event => {
 
-const url = event.request.url;
-
-/* CACHE .bed FILES */
-
-if(url.endsWith(".bed")){
-
 event.respondWith(
 
-caches.open("bed-files").then(cache=>{
+caches.match(event.request)
+.then(response => {
 
-return cache.match(event.request).then(res=>{
+return response || fetch(event.request);
 
-return res || fetch(event.request).then(net=>{
-
-cache.put(event.request, net.clone());
-
-return net;
-
-});
-
-});
-
-})
-
-);
-
-return;
-
-}
-
-/* NORMAL FILES */
-
-event.respondWith(
-
-caches.match(event.request).then(res=>{
-return res || fetch(event.request);
 })
 
 );
