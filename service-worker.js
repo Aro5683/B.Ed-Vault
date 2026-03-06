0b8ef7b74c25eb793dd64411a30e5565f58ffa7f
Aro5683 <arotesting54@gmail.com>
2026-03-06 09:11:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = "bed-vault-cache-v1";
+const CACHE_NAME = "bed-vault-v1";
 
 const urlsToCache = [
 "./",
-"./index.html"
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
 
 event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames.map(cache => {
+if (cache !== CACHE_NAME) {
+return caches.delete(cache);
+}
+})
+);
+})
 );
 
 });
@@ -17,14 +37,9 @@ caches.open(CACHE_NAME)
 self.addEventListener("fetch", event => {
 
 event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-
+caches.match(event.request).then(response => {
 return response || fetch(event.request);
-
 })
-
 );
 
 });
